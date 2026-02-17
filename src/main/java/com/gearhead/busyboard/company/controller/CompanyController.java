4dcaba075f98489495e1f8de816ccf689b517a93
manjubugude <bugude.manjunath@getswarms.com>
2026-02-17 23:05:39 +0000

    added few more company related api's

--- src/main/java/com/gearhead/busyboard/company/controller/CompanyController.java
+++ src/main/java/com/gearhead/busyboard/company/controller/CompanyController.java
@@ -6,10 +6,9 @@ import com.gearhead.busyboard.company.entity.Company;
 import com.gearhead.busyboard.company.service.CompanyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Company")
@@ -24,8 +23,26 @@ public class CompanyController {
     @PostMapping("/create")
     public ResponseEntity<CompanyResponseDTO> createCompany(
             @RequestBody CompanyRequestDTO requestDTO) {
-            CompanyResponseDTO responseDTO= companyService.createCompany(requestDTO);
-            return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+        CompanyResponseDTO responseDTO= companyService.createCompany(requestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
+    @PutMapping("/Update/{id}")
+    public ResponseEntity<CompanyResponseDTO> updateCompany(
+           @PathVariable("id") int id, @RequestBody CompanyRequestDTO companyRequestDTO){
+        CompanyResponseDTO responseDTO=companyService.updateCompany(id, companyRequestDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDTO);
+    }
+
+    @GetMapping("/getList")
+    public  ResponseEntity<List<CompanyResponseDTO>> getAllCompanies(){
+        List<CompanyResponseDTO> listDTO=companyService.getAllCompanies();
+        return ResponseEntity.status(HttpStatus.FOUND).body(listDTO);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<CompanyResponseDTO> getCompany( @PathVariable("id") int id){
+        CompanyResponseDTO companyResponseDTO=companyService.getCompany(id);
+        return ResponseEntity.status(HttpStatus.FOUND).body(companyResponseDTO);
+    }
 }
--- src/main/java/com/gearhead/busyboard/company/service/CompanyService.java
+++ src/main/java/com/gearhead/busyboard/company/service/CompanyService.java
@@ -5,23 +5,23 @@ import com.gearhead.busyboard.company.dto.CompanyResponseDTO;
 import com.gearhead.busyboard.company.entity.Company;
 import com.gearhead.busyboard.company.mapper.CompanyMapper;
 import com.gearhead.busyboard.company.repo.CompanyRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
 import java.time.Instant;
-import java.util.Calendar;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
 public class CompanyService {
 
-    private final CompanyRepository repository;
+    private final CompanyRepository companyRepository;
     private final CompanyMapper mapper;
 
-    public CompanyService(CompanyRepository repository,
+    public CompanyService(CompanyRepository companyRepository,
                           CompanyMapper mapper) {
-        this.repository = repository;
+        this.companyRepository = companyRepository;
         this.mapper = mapper;
     }
 
@@ -30,8 +30,35 @@ public class CompanyService {
         Company company = mapper.toEntity(requestDTO);
         company.setCreatedAt(Instant.now());
 
-        Company saved = repository.save(company);
+        Company saved = companyRepository.save(company);
         return mapper.toResponseDTO(saved);
+    }
+    
+    public CompanyResponseDTO getCompany(int id){
+        Optional<Company> company=companyRepository.findById(id);
+        if (company.isEmpty()){
+            throw new RuntimeException("Company not found with the id:"+id);
+        }
+        return mapper.toResponseDTO(company.get());
+    }
+
+    public CompanyResponseDTO updateCompany(int id, CompanyRequestDTO companyRequestDTO){
+        Company company = companyRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("Company not found with id: " + id));
+
+        Optional.ofNullable(companyRequestDTO.name()).ifPresent(company::setName);
+        Optional.ofNullable(companyRequestDTO.subscriptionPlan()).ifPresent(company::setSubscriptionPlan);
+
+
+        Company updated = companyRepository.save(company);
+
+        return mapper.toResponseDTO(updated);
+    }
+
+    public List<CompanyResponseDTO> getAllCompanies(){
+        List<Company> companies=companyRepository.findAll();
+        return companies.stream().map(mapper::toResponseDTO).toList();
     }
 }
 
